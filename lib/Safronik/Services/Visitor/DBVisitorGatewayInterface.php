b519918be223fb75710d4b8fbb6272726eaba7f8
Roman Safronov <r.safronik@gmail.com>
2024-03-06 14:27:34 +0500

    Del: Visitor service.

--- lib/Safronik/Services/Visitor/DBVisitorGatewayInterface.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Safronik\Services\Visitor;
-
-interface DBVisitorGatewayInterface
-{
-    public function registerVisitor( $id, $ip, $ip_decimal, $user_agent, $browser_signature ): void;
-    public function getVisits( string $id): array;
-}
--- lib/Safronik/Services/Visitor/Interfaces/VariableInterface.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Safronik\Services\Visitor\Interfaces;
-
-interface VariableInterface
-{
-    public function registerVisitor( $id, $ip, $ip_decimal, $user_agent, $browser_signature ): void;
-    public function getVisits( string $id): array;
-}
