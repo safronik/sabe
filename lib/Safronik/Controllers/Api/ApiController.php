aa6214616722e0a5c9cda40f843d3df075b36bf8
safronik <r.safronov@macrodigital.ru>
2025-09-26 19:49:55 +0500

    Middleware: Preparing. Delete controller extensions 2.

--- lib/Safronik/Controllers/Api/ApiController.php
+++ lib/Safronik/Controllers/Api/ApiController.php
@@ -3,16 +3,11 @@
 namespace Safronik\Controllers\Api;
 
 use Safronik\Controllers\Controller;
-use Safronik\Controllers\Extensions\ApiCallLimit\ApiCallLimitExtension;
-use Safronik\Controllers\Extensions\AuthApiKey\AuthApiKeyExtension;
 use Safronik\Router\Routes\AbstractRoute;
 use Safronik\Views\Api\ApiView;
 use Safronik\Views\ViewInterface;
 
 abstract class ApiController extends Controller{
-    
-    use AuthApiKeyExtension;
-    use ApiCallLimitExtension;
 
     protected ViewInterface $view;
 
@@ -21,10 +16,5 @@ abstract class ApiController extends Controller{
         $this->view = $view;
 
         parent::__construct($route);
-    }
-
-    protected function getApiKey(): string
-    {
-        return '';
     }
 }
