703c69d192b13511994309000fcd4dd753e1dd36
safronik <r.safronov@macrodigital.ru>
2025-10-05 21:02:22 +0500

    ErrorHandler and its implementation

--- lib/Safronik/Core/Core.php
+++ lib/Safronik/Core/Core.php
@@ -2,6 +2,7 @@
 
 namespace Safronik\Core;
 
+use Safronik\CodePatterns\Generative\Multiton;
 use Safronik\CodePatterns\Structural\DI;
 use Safronik\DB\DBConfig;
 
@@ -12,56 +13,67 @@ use Safronik\DB\DBConfig;
  */
 readonly class Core
 {
+    // @todo uncomment when I decide to make multiapp
+    // use Multiton;
+
+    private const DIR_RUNTIME = 'runtime';
+    private const DIR_CONFIG = 'runtime';
+
     public function __construct( string $root_dir, array $additional_config = [] )
     {
-        try{
-            $this->configure(
-                $root_dir,
-                $additional_config
-                    ?? [
-                        'dirs' => [
-                            'root'    => $root_dir,
-                            'runtime' => $root_dir . DIRECTORY_SEPARATOR . 'runtime',
-                        ],
-                    ]
-            );
-            $this->setErrorReportingLevel();
-            $this->setupDiContainer();
-            $this->initializeModules();
-        }catch( \Exception $exception ){
-            throw $exception;
-        }
+        $this->initConfig( $root_dir, $additional_config );
+        $this->initErrorHandler();
+        $this->initExceptionHandler();
+        $this->initDiContainer();
+        $this->initModules();
     }
-    
-    private function configure( string $root_dir, array $additional_config ): void
+
+    /**
+     * Initializes configuration
+     * Uploads config into memory so it can be accessed by Config::get('config.request.by.path')
+     *
+     * @param string $root_dir          Absolute path to the root directory (could be different for different apps)
+     * @param array  $additional_config Additional config to be merged with the default config
+     */
+    private function initConfig( string $root_dir, array $additional_config = [] ): void
     {
         Config::initialize(
-            $root_dir . DIRECTORY_SEPARATOR . 'config',
-            $additional_config
+            $root_dir . DIRECTORY_SEPARATOR . self::DIR_CONFIG,
+            array_merge(
+                [
+                    'dirs' => [
+                        'root'    => $root_dir,
+                        'runtime' => $root_dir . DIRECTORY_SEPARATOR . self::DIR_RUNTIME,
+                    ],
+                ],
+                $additional_config
+            )
         );
     }
-    
-    private function setErrorReportingLevel(): void
+
+    /**
+     * Initializes error handling
+     * Initializes error logger
+     *
+     * @return void
+     *
+     * @throws Exceptions\ConfigException
+     *
+     */
+    private function initErrorHandler(): void
     {
-        switch( Config::get('options.mode') ){
-            case 'production':
-                error_reporting(0 );
-                ini_set('display_errors', 'Off');
-                break;
-            case 'stage':
-                // @todo implement
-                break;
-            case 'dev':
-                error_reporting( E_ALL ^ E_DEPRECATED );
-                ini_set( 'display_errors', 'On' );
-                break;
-            default:
-                error_reporting( E_ALL ^ E_DEPRECATED );
-                ini_set( 'display_errors', 'On' );
-        }
+        new ErrorHandler(
+            Config::get('options.mode'),
+            Config::get('options.error_handler_custom_params')
+        );
     }
-    
-    private function setupDiContainer(): void
+
+    private function initExceptionHandler(): void
+    {
+
+    }
+
+    private function initDiContainer(): void
     {
 //         $vendor_safronik_dir = $root_dir . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'safronik';
 //         $di_class_map = array_merge(
@@ -69,20 +81,22 @@ readonly class Core
 //             DI::getClassMapForDirectory( $vendor_safronik_dir . DIRECTORY_SEPARATOR . 'db-wrapper' . DIRECTORY_SEPARATOR . 'src', '\Safronik\DB' ),
 //             DI::getClassMapForDirectory( $vendor_safronik_dir . DIRECTORY_SEPARATOR . 'db-migrator' . DIRECTORY_SEPARATOR . 'src', '\Safronik\DBMigrator' ),
 //         );
+
         DI::initialize(
             Config::get('di.class_map'),
             Config::get('di.interface_map')
         );
+
         DI::setParametersFor(
             DBConfig::class,
             [ Config::get( 'db.mysql' ) ]
         );
     }
-    
+
     /**
      * Initialize services
      */
-    private function initializeModules(): void
+    private function initModules(): void
     {
 
     }
